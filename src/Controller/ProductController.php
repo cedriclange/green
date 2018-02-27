bdb13b83ffd81b7d099ba57f80e01eeb13f13fdf
Lange Guru <langemataso@gmail.com>
2018-02-27 10:10:22 +0200

    single function in product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,16 +4,25 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Product;
+use App\Entity\Category;
 
 class ProductController extends Controller
 {
     /**
-     * @Route("/product", name="product")
+     * @Route("/product", name="product_list")
      */
     public function index()
     {
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
         ]);
+    }
+    /**
+     * @Route("/product/{id}, name="show_product")
+     */
+    public function getSingle($id)
+    {
+
     }
 }
